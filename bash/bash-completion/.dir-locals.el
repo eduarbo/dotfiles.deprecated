7c7580ab11490d8c9c8c2bb398019776ee2094ad
Eduardo Ruiz <eduarbo@gmail.com>
2014-10-26 06:14:54 +0000

    remove bash-completion subtree

--- bash/bash-completion/.dir-locals.el
+++
@@ -1,5 +0,0 @@
-;; bash-completion shell-script mode settings for Emacs
-
-((sh-mode . ((indent-tabs-mode  . nil)
-             (sh-basic-offset   . 4)
-             (sh-indent-comment . t))))
--- bash/bash-completion/test/fixtures/java/a/c/README.txt
+++
@@ -1,2 +0,0 @@
-When CLASSPATH is set to the fixtures/java/a dir, we do *not* expect
-*.class in subdirs to be included in completions, see Debian bug #496828.
