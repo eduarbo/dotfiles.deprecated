e9efd57a4cdd2a6af10c975c0956b7803c552201
eduarbo <eduarbo@gmail.com>
2016-02-24 13:40:45 -0800

    Exclude evil-search-hightlight-persist package

--- spacemacs.d/init.el
+++ spacemacs.d/init.el
@@ -53,7 +53,8 @@ values."
    ;; configuration in `dotspacemacs/user-config'.
    dotspacemacs-additional-packages '()
    ;; A list of packages and/or extensions that will not be install and loaded.
-   dotspacemacs-excluded-packages '(evil-escape)
+   dotspacemacs-excluded-packages '(evil-escape
+                                    evil-search-highlight-persist)
    ;; If non-nil spacemacs will delete any orphan packages, i.e. packages that
    ;; are declared in a layer which is not a member of
    ;; the list `dotspacemacs-configuration-layers'. (default t)
