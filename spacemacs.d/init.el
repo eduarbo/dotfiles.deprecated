4f02ab26ec43e70eabacdd8863c8281b21bf652e
eduarbo <eduarbo@gmail.com>
2016-02-26 01:42:26 -0800

    Add python layer

--- spacemacs.d/init.el
+++ spacemacs.d/init.el
@@ -49,6 +49,8 @@ values."
      ;; spell-checking
      syntax-checking
      osx
+
+     python
      )
    ;; List of additional packages that will be installed without being
    ;; wrapped in a layer. If you need some configuration for these
