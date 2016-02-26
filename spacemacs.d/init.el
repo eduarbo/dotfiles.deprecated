79c8aa6a0231dd89a4bfc4e7f98d8c9fd199b3f0
eduarbo <eduarbo@gmail.com>
2016-02-26 12:45:42 -0800

    add sql layer

--- spacemacs.d/init.el
+++ spacemacs.d/init.el
@@ -59,6 +59,7 @@ values."
      html
      javascript
      react
+     sql
      )
    ;; List of additional packages that will be installed without being
    ;; wrapped in a layer. If you need some configuration for these
