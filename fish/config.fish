cb3547657552a75627ca84282b21c4f548bfb19e
eduarbo <eduarbo@gmail.com>
2016-02-29 20:16:04 -0800

    Make fish the default shell
    - add fish config files
    - move aliases to bash/ and remove fish incompatible aliases
    - source bash/aliases in fish, zsh and bash shells
    - add script to start gpg-agent correctly

---
+++ fish/config.fish
@@ -0,0 +1,6 @@
+set -g -x PATH ~/bin $PATH
+set fish_greeting "GLaDOS v1.09 (c) 1982 Aperture Science, Inc."
+set fisher_home ~/.local/share/fisherman
+set fisher_config ~/.config/fisherman
+source $fisher_home/config.fish
+source ~/.dotfiles/bash/aliases
