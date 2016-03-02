06e795d0b03946e39613ea779b478e4f952b5433
eduarbo <eduarbo@gmail.com>
2016-03-02 00:13:21 -0800

    refactor hs init file to move out duplicated code

--- hammerspoon/init.lua
+++ hammerspoon/init.lua
@@ -10,202 +10,130 @@ require("hs.application")
 require("hs.window")
 
 -----------------------------------------------
--- hyper d for left one half window
------------------------------------------------
-
-hs.hotkey.bind(hyper, 'd', function()
-    if hs.window.focusedWindow() then
-        local win = hs.window.focusedWindow()
-        local f = win:frame()
-        local screen = win:screen()
-        local max = screen:frame()
-
-        f.x = max.x
-        f.y = max.y
-        f.w = max.w / 2
-        f.h = max.h
-        win:setFrame(f)
-    else
-        hs.alert.show("No active window")
-    end
-end)
-
------------------------------------------------
--- hyper g for right one half window
------------------------------------------------
-
-hs.hotkey.bind(hyper, 'g', function()
-    if hs.window.focusedWindow() then
-        local win = hs.window.focusedWindow()
-        local f = win:frame()
-        local screen = win:screen()
-        local max = screen:frame()
-
-        f.x = max.x + (max.w / 2)
-        f.y = max.y
-        f.w = max.w / 2
-        f.h = max.h
-        win:setFrame(f)
-    else
-        hs.alert.show("No active window")
-    end
-end)
-
------------------------------------------------
--- hyper f for fullscreen
------------------------------------------------
-
-hs.hotkey.bind(hyper, 'f', function()
-    if hs.window.focusedWindow() then
-        local win = hs.window.focusedWindow()
-        local f = win:frame()
-        local screen = win:screen()
-        local max = screen:frame()
-
-        f.x = max.x
-        f.y = max.y
-        f.w = max.w
-        f.h = max.h
-        win:setFrame(f)
-    else
-        hs.alert.show("No active window")
-    end
-end)
-
------------------------------------------------
--- hyper r for top left one quarter window
------------------------------------------------
-
-hs.hotkey.bind(hyper, 'r', function()
-    if hs.window.focusedWindow() then
-        local win = hs.window.focusedWindow()
-        local f = win:frame()
-        local screen = win:screen()
-        local max = screen:frame()
-
-        f.x = max.x
-        f.y = max.y
-        f.w = max.w / 2
-        f.h = max.h / 2
-        win:setFrame(f)
-    else
-        hs.alert.show("No active window")
-    end
-end)
-
------------------------------------------------
--- hyper t for top right one quarter window
------------------------------------------------
-
-hs.hotkey.bind(hyper, 't', function()
-    if hs.window.focusedWindow() then
-        local win = hs.window.focusedWindow()
-        local f = win:frame()
-        local screen = win:screen()
-        local max = screen:frame()
-
-        f.x = max.x + (max.w / 2)
-        f.y = max.y
-        f.w = max.w / 2
-        f.h = max.h / 2
-        win:setFrame(f)
-    else
-        hs.alert.show("No active window")
-    end
-end)
-
------------------------------------------------
--- hyper v for bottom left one quarter window
------------------------------------------------
-
-hs.hotkey.bind(hyper, 'v', function()
-    if hs.window.focusedWindow() then
-        local win = hs.window.focusedWindow()
-        local f = win:frame()
-        local screen = win:screen()
-        local max = screen:frame()
-
-        f.x = max.x + (max.w / 2)
-        f.y = max.y + (max.h / 2)
-        f.w = max.w / 2
-        f.h = max.h / 2
-        win:setFrame(f)
-    else
-        hs.alert.show("No active window")
-    end
-end)
-
------------------------------------------------
--- hyper c for bottom right one quarter window
------------------------------------------------
-
-hs.hotkey.bind(hyper, 'c', function()
-    if hs.window.focusedWindow() then
-        local win = hs.window.focusedWindow()
-        local f = win:frame()
-        local screen = win:screen()
-        local max = screen:frame()
-
-        f.x = max.x
-        f.y = max.y + (max.h / 2)
-        f.w = max.w / 2
-        f.h = max.h / 2
-        win:setFrame(f)
-    else
-        hs.alert.show("No active window")
-    end
-end)
-
------------------------------------------------
 -- Reload config on write
 -----------------------------------------------
 
 function reload_config(files)
-    hs.reload()
+  hs.reload()
 end
 hs.pathwatcher.new(os.getenv("HOME") .. "/.hammerspoon/", reload_config):start()
 hs.alert.show("Config loaded")
 
 -----------------------------------------------
--- Hyper i to show window hints
+-- Handy functions
 -----------------------------------------------
 
-hs.hotkey.bind(hyper, 'i', function()
-    hs.hints.windowHints()
-end)
+function focusedWindow (fn)
+  return function()
+    -- Workaround to select windows without a title bar
+    local win = hs.window.focusedWindow() or hs.window.frontmostWindow()
+    if win == nil then
+      hs.alert.show("No active window")
+    else
+      fn(win, win:frame(), win:screen():frame())
+    end
+  end
+end
+-----------------------------------------------
+-- hyper d for left one half window
+-----------------------------------------------
+
+hs.hotkey.bind(hyper, 'd', focusedWindow(function(win, f, max)
+  f.x = max.x
+  f.y = max.y
+  f.w = max.w / 2
+  f.h = max.h
+  win:setFrame(f)
+end))
+
+-----------------------------------------------
+-- hyper g for right one half window
+-----------------------------------------------
+
+hs.hotkey.bind(hyper, 'g', focusedWindow(function(win, f, max)
+  f.x = max.x + (max.w / 2)
+  f.y = max.y
+  f.w = max.w / 2
+  f.h = max.h
+  win:setFrame(f)
+end))
+
+-----------------------------------------------
+-- hyper f for fullscreen
+-----------------------------------------------
+
+hs.hotkey.bind(hyper, 'f', focusedWindow(function(win, f, max)
+  f.x = max.x
+  f.y = max.y
+  f.w = max.w
+  f.h = max.h
+  win:setFrame(f)
+end))
+
+-----------------------------------------------
+-- hyper r for top left one quarter window
+-----------------------------------------------
+
+hs.hotkey.bind(hyper, 'r', focusedWindow(function(win, f, max)
+  f.x = max.x
+  f.y = max.y
+  f.w = max.w / 2
+  f.h = max.h / 2
+  win:setFrame(f)
+end))
+
+-----------------------------------------------
+-- hyper t for top right one quarter window
+-----------------------------------------------
+
+hs.hotkey.bind(hyper, 't', focusedWindow(function(win, f, max)
+  f.x = max.x + (max.w / 2)
+  f.y = max.y
+  f.w = max.w / 2
+  f.h = max.h / 2
+  win:setFrame(f)
+end))
+
+-----------------------------------------------
+-- hyper v for bottom left one quarter window
+-----------------------------------------------
+
+hs.hotkey.bind(hyper, 'v', focusedWindow(function(win, f, max)
+  f.x = max.x + (max.w / 2)
+  f.y = max.y + (max.h / 2)
+  f.w = max.w / 2
+  f.h = max.h / 2
+  win:setFrame(f)
+end))
+
+-----------------------------------------------
+-- hyper c for bottom right one quarter window
+-----------------------------------------------
+
+hs.hotkey.bind(hyper, 'c', focusedWindow(function(win, f, max)
+  f.x = max.x
+  f.y = max.y + (max.h / 2)
+  f.w = max.w / 2
+  f.h = max.h / 2
+  win:setFrame(f)
+end))
 
 -----------------------------------------------
 -- Hyper hjkl to switch window focus
 -----------------------------------------------
 
-hs.hotkey.bind(hyper, 'k', function()
-    if hs.window.focusedWindow() then
-        hs.window.focusedWindow():focusWindowNorth()
-    else
-        hs.alert.show("No active window")
-    end
-end)
+hs.hotkey.bind(hyper, 'k', focusedWindow(function(win)
+  win:focusWindowNorth()
+end))
 
-hs.hotkey.bind(hyper, 'j', function()
-    if hs.window.focusedWindow() then
-        hs.window.focusedWindow():focusWindowSouth()
-    else
-        hs.alert.show("No active window")
-    end
-end)
+hs.hotkey.bind(hyper, 'j', focusedWindow(function(win)
+  win:focusWindowSouth()
+end))
 
-hs.hotkey.bind(hyper, 'l', function()
-    if hs.window.focusedWindow() then
-    hs.window.focusedWindow():focusWindowEast()
-    else
-        hs.alert.show("No active window")
-    end
-end)
+hs.hotkey.bind(hyper, 'l', focusedWindow(function(win)
+  win:focusWindowEast()
+end))
 
-hs.hotkey.bind(hyper, 'h', function()
-    if hs.window.focusedWindow() then
-        hs.window.focusedWindow():focusWindowWest()
-    else
-        hs.alert.show("No active window")
-    end
-end)
+hs.hotkey.bind(hyper, 'h', focusedWindow(function(win)
+  win:focusWindowWest()
+end))
