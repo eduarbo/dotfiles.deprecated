d156c724e52e077c7ad9adce67a1177a3c1d1c4c
eduarbo <eduarbo@gmail.com>
2016-03-01 21:31:00 -0800

    Use bass to run the `init-gpg-agent` script

--- fish/config.fish
+++ fish/config.fish
@@ -1,6 +1,8 @@
 set -g -x PATH ~/bin $PATH
+set DOT ~/.dotfiles
 set fish_greeting "GLaDOS v1.09 (c) 1982 Aperture Science, Inc."
 set fisher_home ~/.local/share/fisherman
 set fisher_config ~/.config/fisherman
 source $fisher_home/config.fish
-source ~/.dotfiles/bash/aliases
+source $DOT/bash/aliases
+bass source $DOT/bin/init-gpg-agent
