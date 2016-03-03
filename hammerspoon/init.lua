4920ae504446b40d9010b4d3a9594f2b7fed1442
eduarbo <eduarbo@gmail.com>
2016-03-02 20:31:57 -0800

    more hotkeys to launch screensaver, hs console...

--- hammerspoon/init.lua
+++ hammerspoon/init.lua
@@ -2,12 +2,14 @@
 -- Set up
 -----------------------------------------------
 
--- local hyper = {"shift", "cmd", "alt", "ctrl"}
-local hyper = {"shift", "alt"}
+local MEGA = {"cmd", "alt", "ctrl"}
+local HYPER = {"cmd", "alt", "ctrl", "shift"}
 hs.window.animationDuration = 0
 
 require("hs.application")
 require("hs.window")
+require("hs.layout")
+require("hs.caffeinate")
 
 -----------------------------------------------
 -- Reload config on write
@@ -17,7 +19,7 @@ function reload_config(files)
   hs.reload()
 end
 hs.pathwatcher.new(os.getenv("HOME") .. "/.hammerspoon/", reload_config):start()
-hs.alert.show("Config loaded")
+hs.alert("Config loaded")
 
 -----------------------------------------------
 -- Handy functions
@@ -28,112 +30,95 @@ function focusedWindow (fn)
     -- Workaround to select windows without a title bar
     local win = hs.window.focusedWindow() or hs.window.frontmostWindow()
     if win == nil then
-      hs.alert.show("No active window")
+      hs.alert("No active window")
     else
-      fn(win, win:frame(), win:screen():frame())
+      fn(win)
     end
   end
 end
+
 -----------------------------------------------
--- hyper d for left one half window
+-- Move and Resize windows with modifier Shift+CMD
 -----------------------------------------------
 
-hs.hotkey.bind(hyper, 'd', focusedWindow(function(win, f, max)
-  f.x = max.x
-  f.y = max.y
-  f.w = max.w / 2
-  f.h = max.h
-  win:setFrame(f)
+local winmod = {
+  -- Halves
+  { 'h', hs.layout.left50 },  -- left half
+  { 'l', hs.layout.right50 }, -- right half
+  { 'k', {0,0,1,0.5} },       -- top half
+  { 'j', {0,0.5,1,0.5} },     -- bottom half
+  --quadrants
+  { 'i', {0,0,1/2,1/2} },     -- left top corner
+  { 'o', {1/2,0,1/2,1/2} },   -- right top corner
+  { 'u', {0,1/2,1/2,1/2} },   -- left bottom corner
+  { 'p', {1/2,1/2,1/2,1/2} }, -- right bottom corner
+}
+
+for i,item in ipairs(winmod) do
+  hs.hotkey.bind(HYPER, item[1], focusedWindow(function(win)
+    win:move(item[2])
+  end))
+end
+
+-----------------------------------------------
+-- fullscreen
+-----------------------------------------------
+
+local frameCache = {}
+hs.hotkey.bind(HYPER, 'f', focusedWindow(function(win)
+  -- win:move(hs.layout.maximized)
+  if frameCache[win:id()] then
+    win:setFrame(frameCache[win:id()])
+    frameCache[win:id()] = nil
+  else
+    frameCache[win:id()] = win:frame()
+    win:maximize()
+  end
 end))
 
 -----------------------------------------------
--- hyper g for right one half window
+-- Launcher with mega modifier
 -----------------------------------------------
-
-hs.hotkey.bind(hyper, 'g', focusedWindow(function(win, f, max)
-  f.x = max.x + (max.w / 2)
-  f.y = max.y
-  f.w = max.w / 2
-  f.h = max.h
-  win:setFrame(f)
-end))
-
+-- In the following example, the list `{ 'e', "Emacs", true }` holds the key
+-- to be used with the modifier to launch the application described in the 2nd
+-- position. The 3rd position (optional) is used as workaround to bring
+-- apps compiled without a title bar (like my emacs version <3) at front.
 -----------------------------------------------
--- hyper f for fullscreen
------------------------------------------------
+local apps = {
+  { 'a', "Calendar" },
+  { 'b', "/Applications/FirefoxDeveloperEdition.app" },
+  { 'e', "Emacs", true },
+  { 'i', "iTerm" },
+  { 's', "Skype" },
+  { 'v', "VLC" },
+  { 'f', "Finder" },
+  { 'm', "Mail" },
+  { 'd', "Slack" },
+  { 'p', "Spotify" },
+  { 'r', "Karabiner" },
+}
 
-hs.hotkey.bind(hyper, 'f', focusedWindow(function(win, f, max)
-  f.x = max.x
-  f.y = max.y
-  f.w = max.w
-  f.h = max.h
-  win:setFrame(f)
-end))
+for i,item in ipairs(apps) do
+  hs.hotkey.bind(MEGA, item[1], function()
+    local app = hs.application.open(item[2])
+    if app then app:activate(true) end
+  end)
+end
 
------------------------------------------------
--- hyper r for top left one quarter window
------------------------------------------------
+-- Open Hammerspoon Console
+hs.hotkey.bind(MEGA, 'o', function() hs.openConsole() end)
 
-hs.hotkey.bind(hyper, 'r', focusedWindow(function(win, f, max)
-  f.x = max.x
-  f.y = max.y
-  f.w = max.w / 2
-  f.h = max.h / 2
-  win:setFrame(f)
-end))
-
------------------------------------------------
--- hyper t for top right one quarter window
------------------------------------------------
-
-hs.hotkey.bind(hyper, 't', focusedWindow(function(win, f, max)
-  f.x = max.x + (max.w / 2)
-  f.y = max.y
-  f.w = max.w / 2
-  f.h = max.h / 2
-  win:setFrame(f)
-end))
+-- Launch Screensaver
+hs.hotkey.bind(MEGA, '`', function()
+  hs.caffeinate.startScreensaver()
+end)
 
 -----------------------------------------------
--- hyper v for bottom left one quarter window
+-- Date & Time
 -----------------------------------------------
 
-hs.hotkey.bind(hyper, 'v', focusedWindow(function(win, f, max)
-  f.x = max.x + (max.w / 2)
-  f.y = max.y + (max.h / 2)
-  f.w = max.w / 2
-  f.h = max.h / 2
-  win:setFrame(f)
-end))
-
------------------------------------------------
--- hyper c for bottom right one quarter window
------------------------------------------------
-
-hs.hotkey.bind(hyper, 'c', focusedWindow(function(win, f, max)
-  f.x = max.x
-  f.y = max.y + (max.h / 2)
-  f.w = max.w / 2
-  f.h = max.h / 2
-  win:setFrame(f)
-end))
-
------------------------------------------------
--- Hyper hjkl to switch window focus
------------------------------------------------
-
-hs.hotkey.bind(hyper, 'k', focusedWindow(function(win)
-  win:focusWindowNorth()
-end))
-
-hs.hotkey.bind(hyper, 'j', focusedWindow(function(win)
-  win:focusWindowSouth()
-end))
-
-hs.hotkey.bind(hyper, 'l', focusedWindow(function(win)
-  win:focusWindowEast()
-end))
-
-hs.hotkey.bind(hyper, 'h', focusedWindow(function(win)
-  win:focusWindowWest()
-end))
+hs.hotkey.bind(MEGA, '/', function()
+  local date = os.date('%A, %h %e')
+  local time = os.date('%I:%M%p'):gsub('^0',''):lower()
+  hs.alert(time..' - '..date, 2.5)
+end)
