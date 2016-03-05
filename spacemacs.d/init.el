252328c575c80972fbc9686b3aaac5e3694b4965
eduarbo <eduarbo@gmail.com>
2016-03-04 20:21:37 -0800

    Don't not maximize emacs at startup anymore

--- spacemacs.d/init.el
+++ spacemacs.d/init.el
@@ -225,7 +225,7 @@ values."
    ;; If non nil the frame is maximized when Emacs starts up.
    ;; Takes effect only if `dotspacemacs-fullscreen-at-startup' is nil.
    ;; (default nil) (Emacs 24.4+ only)
-   dotspacemacs-maximized-at-startup t
+   dotspacemacs-maximized-at-startup nil
    ;; A value from the range (0..100), in increasing opacity, which describes
    ;; the transparency level of a frame when it's active or selected.
    ;; Transparency can be toggled through `toggle-transparency'. (default 90)
@@ -464,6 +464,5 @@ already narrowed."
                             prog-mode-hook))
   ;; Break lines automatically
   (spacemacs/add-to-hooks 'spacemacs/toggle-visual-line-navigation-on
-                          '(org-mode-hook
-                            prog-mode-hook))
+                          '(org-mode-hook))
   )
