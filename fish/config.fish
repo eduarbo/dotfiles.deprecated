5082eb24ecb24cf8c227764bbe3a005a34a7eadf
eduarbo <eduarbo@gmail.com>
2016-03-04 20:17:14 -0800

    set PATH correctly to not prepend values on each fish instance loaded

--- fish/config.fish
+++ fish/config.fish
@@ -1,8 +1,13 @@
-set -g -x PATH ~/bin $PATH
+set -U fish_user_paths ~/bin $fish_user_paths /usr/local/sbin
 set DOT ~/.dotfiles
 set fish_greeting "GLaDOS v1.09 (c) 1982 Aperture Science, Inc."
+
+source $DOT/bash/aliases
+if type bass >/dev/null ^/dev/null
+  # bass allows me to source bash scripts
+  bass source $DOT/bin/init-gpg-agent
+end
+
 set fisher_home ~/.local/share/fisherman
 set fisher_config ~/.config/fisherman
 source $fisher_home/config.fish
-source $DOT/bash/aliases
-bass source $DOT/bin/init-gpg-agent
