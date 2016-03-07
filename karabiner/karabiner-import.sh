ae5097be1cd7ddfc2b7900a6f07e95372d2a3298
eduarbo <eduarbo@gmail.com>
2016-03-07 06:57:12 -0800

    script to export karabiner settings

---
+++ karabiner/karabiner-import.sh
@@ -0,0 +1,115 @@
+#!/bin/sh
+
+cli=/Applications/Karabiner.app/Contents/Library/bin/karabiner
+
+$cli set remap.fn_fkeys_to_consumer_f1 1
+/bin/echo -n .
+$cli set remap.optionR2controlR_return_keyrepeat 1
+/bin/echo -n .
+$cli set private.switch_profile 1
+/bin/echo -n .
+$cli set space_cadet.f19_to_delete 1
+/bin/echo -n .
+$cli set private.option_L_to_Super 1
+/bin/echo -n .
+$cli set parameter.keyoverlaidmodifier_timeout 300
+/bin/echo -n .
+$cli set private.super_custom_shortcuts 1
+/bin/echo -n .
+$cli set space_cadet.left_control_to_hyper 1
+/bin/echo -n .
+$cli set remap.fn_fkeys_to_consumer_f5 1
+/bin/echo -n .
+$cli set private.mega_n_add_tilde 1
+/bin/echo -n .
+$cli set remap.command_to_command_brackets 1
+/bin/echo -n .
+$cli set remap.fn_fkeys_to_consumer_f7 1
+/bin/echo -n .
+$cli set repeat.keyoverlaidmodifier_initial_wait 400
+/bin/echo -n .
+$cli set repeat.initial_wait 300
+/bin/echo -n .
+$cli set remap.fn_fkeys_to_consumer_f3_lion 1
+/bin/echo -n .
+$cli set private.super_j_to_down 1
+/bin/echo -n .
+$cli set remap.fn_fkeys_to_consumer_f10 1
+/bin/echo -n .
+$cli set private.shifts_to_parens 1
+/bin/echo -n .
+$cli set repeat.wait 20
+/bin/echo -n .
+$cli set remap.controlR2fn 1
+/bin/echo -n .
+$cli set private.hyper_to_homerow_numbers 1
+/bin/echo -n .
+$cli set private.controlR_controlL2Hyper 1
+/bin/echo -n .
+$cli set space_cadet.f19_to_hyper 1
+/bin/echo -n .
+$cli set space_cadet.space_to_modifier 1
+/bin/echo -n .
+$cli set private.hyper_custom_shortcuts 1
+/bin/echo -n .
+$cli set private.f-zero 1
+/bin/echo -n .
+$cli set private.option_R_to_mega 1
+/bin/echo -n .
+$cli set remap.space_to_control 1
+/bin/echo -n .
+$cli set private.super_hjkl_to_left_down_up_right 1
+/bin/echo -n .
+$cli set private.super_n_add_tilde 1
+/bin/echo -n .
+$cli set private.shift_space_to_Hyper 1
+/bin/echo -n .
+$cli set private.super_to_command_shift_brackets 1
+/bin/echo -n .
+$cli set private.hyper_to_command_shift_brackets 1
+/bin/echo -n .
+$cli set private.option_L_to_mega 1
+/bin/echo -n .
+$cli set remap.option2control_return_keyrepeat 1
+/bin/echo -n .
+$cli set private.super_l_to_right 1
+/bin/echo -n .
+$cli set private.super_h_to_left 1
+/bin/echo -n .
+$cli set private.option_key_to_Super 1
+/bin/echo -n .
+$cli set remap.doublepresscommandQ 1
+/bin/echo -n .
+$cli set private.f19_to_delete 1
+/bin/echo -n .
+$cli set remap.controlL2optionL 1
+/bin/echo -n .
+$cli set space_cadet.f19_and_return_to_hyper 1
+/bin/echo -n .
+$cli set private.super_semicolon_add_tilde 1
+/bin/echo -n .
+$cli set private.modifiers_to_Hyper 1
+/bin/echo -n .
+$cli set private.option_R_to_Super 1
+/bin/echo -n .
+$cli set private.super_k_to_up 1
+/bin/echo -n .
+$cli set f_zero.enable 1
+/bin/echo -n .
+$cli set private.option_key_to_Hyper 1
+/bin/echo -n .
+$cli set private.option_R_to_Hyper 1
+/bin/echo -n .
+$cli set metamorphium.hyper 1
+/bin/echo -n .
+$cli set private.mega_hjkl_to_left_down_up_right 1
+/bin/echo -n .
+$cli set private.mega_semicolon_add_tilde 1
+/bin/echo -n .
+$cli set remap.pc_application2optionR 1
+/bin/echo -n .
+$cli set private.mega_to_command_shift_brackets 1
+/bin/echo -n .
+$cli set space_cadet.space_to_hyper 1
+/bin/echo -n .
+/bin/echo
