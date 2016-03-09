71bb9fb9c59f02eca9a827845f3650e6deee5e42
eduarbo <eduarbo@gmail.com>
2016-03-08 18:33:45 -0800

    update kwmc path

--- kwm/.kwm/launch_or_focus.sh
+++ kwm/.kwm/launch_or_focus.sh
@@ -3,4 +3,7 @@
 open -a "$1"
 
 # Find oldest process id to focus it with kwm
-/usr/local/bin/kwmc window -f id $(ps aux | grep -v grep | grep -i "$1" | awk '{print $2;}' | sort -n | head -1)
+win_id=$(/Users/eduarbo/bin/kwmc read windows | grep -i "$1" | head -1 | awk '{print $1;}')
+# remove trailing comma
+win_id=${win_id%?}
+/Users/eduarbo/bin/kwmc window -f id $win_id
