5a64749c1d20397ed157a298779905ef9ae69d1c
eduarbo <eduarbo@gmail.com>
2016-10-18 04:17:03 -0500

    More functions for note private layer

--- spacemacs/.spacemacs.d/layers/notes/funcs.el
+++ spacemacs/.spacemacs.d/layers/notes/funcs.el
@@ -8,11 +8,24 @@
 ;;
 ;;; License: GPLv3
 
-(defun notes-journal-path ()
+(cl-defun notes-journal-path (&optional (offset 0))
   "Path to the note of the day"
-  (concat notes-directory (format-time-string "%Y%m%d.org")))
+  (let* ((date (time-add (current-time) (* offset 60 60 24)))
+         (dir (concat deft-directory (format-time-string "journal/%Y/" date))))
+    (make-directory dir t)
+    (concat dir (format-time-string "%Y-%m-%d.org" date))))
 
-(defun notes-open-journal ()
+(defun notes-open-tomorrow-journal ()
+  "Open note of the day"
+  (interactive)
+  (find-file (notes-journal-path 1)))
+
+(defun notes-open-yesterday-journal ()
+  "Open note of the day"
+  (interactive)
+  (find-file (notes-journal-path -1)))
+
+(defun notes-open-today-journal ()
   "Open note of the day"
   (interactive)
   (find-file (notes-journal-path)))
@@ -21,6 +34,12 @@
   "Open my secrets file"
   (interactive)
   (find-file notes-secrets-path))
+
+(defun deft-toggle-recursive-search ()
+  "Toggle recursive search for files in subdirectories"
+  (interactive)
+  (setq deft-recursive (if deft-recursive nil t))
+  (deft-refresh))
 
 (defun notes-journal-file-insert ()
   "Insert's the journal heading based on the file's name."
