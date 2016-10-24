9c588a67ac2bd677426977ad3b5e1da3318c0417
eduarbo <eduarbo@gmail.com>
2016-10-24 18:58:31 -0500

    Create and add private pretty-symbols layer

--- spacemacs/.spacemacs.d/init.el
+++ spacemacs/.spacemacs.d/init.el
@@ -99,7 +99,9 @@ values."
      (crypt :variables
             crypt-gpg-key "eduarbo@gmail.com")
      notes
-     defaults)
+     defaults
+     pretty-symbols
+     )
    ;; List of additional packages that will be installed without being
    ;; wrapped in a layer. If you need some configuration for these
    ;; packages, then consider creating a layer. You can also put the
