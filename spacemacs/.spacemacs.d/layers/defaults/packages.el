d03be4c855fcf1ee5b4dcec23b347a240b138755
eduarbo <eduarbo@gmail.com>
2016-10-24 02:28:59 -0500

    sort by alphabetic order functions in defaults/packages.el

--- spacemacs/.spacemacs.d/layers/defaults/packages.el
+++ spacemacs/.spacemacs.d/layers/defaults/packages.el
@@ -27,87 +27,10 @@
     undo-tree
     yasnippet))
 
-(defun defaults/post-init-evil ()
-  (spacemacs/set-leader-keys
-    "TAB" 'evil-switch-to-windows-last-buffer)
-  (bind-map-set-keys evil-motion-state-map
-    "C-h" 'evil-window-left
-    "C-j" 'evil-window-down
-    "C-k" 'evil-window-up
-    "C-l" 'evil-window-right)
-  (bind-map-set-keys evil-normal-state-map
-    ":"       'evil-repeat-find-char-reverse
-    "<C-tab>" 'evil-jump-item))
-
 (defun defaults/post-init-avy ()
   (bind-map-set-keys evil-normal-state-map
     "s" 'avy-goto-char-timer
     "S" 'avy-goto-char-in-line))
-
-(defun defaults/post-init-evil-args ()
-  (bind-map-set-keys evil-normal-state-map
-    "L" 'evil-forward-arg
-    "H" 'evil-backward-arg)
-  (bind-map-set-keys evil-motion-state-map
-    "L" 'evil-forward-arg
-    "H" 'evil-backward-arg))
-
-(defun defaults/post-init-helm ()
-  ;; Enable fuzzy matching for everything
-  (setq helm-completion-in-region-fuzzy-match t
-        helm-mode-fuzzy-match t))
-
-(defun defaults/post-init-helm-dash ()
-  (setq helm-dash-browser-func 'eww))
-
-(defun defaults/post-init-helm-projectile ()
-  (spacemacs/set-leader-keys
-    "/" 'spacemacs/helm-project-do-grep))
-
-(defun defaults/post-init-web-mode ()
-  (setq css-indent-offset tab-width
-        web-mode-markup-indent-offset tab-width
-        web-mode-css-indent-offset tab-width
-        web-mode-code-indent-offset tab-width
-        web-mode-attr-indent-offset tab-width))
-
-(defun defaults/post-init-js2-mode ()
-  ;; Let flycheck handle parse errors
-  (setq js2-mode-show-parse-errors nil
-        js2-mode-show-strict-warnings nil)
-
-  ;; Force js to indent 2 levels
-  (setq js-indent-level tab-width
-        js2-basic-offset tab-width))
-
-(defun defaults/post-init-sh-script ()
-  ;; Use same indentation spaces than 'tab-width
-  (setq sh-indentation tab-width
-        sh-basic-offset tab-width))
-
-(defun defaults/post-init-gruvbox ()
-  ;; Darker vertical-border for gruvbox
-  (set-face-attribute 'vertical-border nil
-                      :foreground "#1d2021" :background nil))
-
-(defun defaults/post-init-subatomic-theme ()
-  (set-face-attribute 'vertical-border nil
-                      :foreground "#232533")
-  (set-face-attribute 'mode-line nil
-                      :box '(:line-width 1 :color "#232533"))
-  (set-face-attribute 'modeline-inactive nil
-                      :box '(:line-width 1 :color "#2a2c3e")))
-
-(defun defaults/post-init-spaceline ()
-  (setq powerline-default-separator 'utf-8)
-  (custom-set-variables '(powerline-utf-8-separator-left #xe0b0)
-                        '(powerline-utf-8-separator-right #xe0b2))
-  ;; I need to compile spaceline to take the changes
-  (spaceline-compile))
-
-(defun defaults/post-init-undo-tree ()
-  (setq undo-tree-auto-save-history t
-        undo-tree-history-directory-alist '(("." . "~/.emacs.d/.cache/undo"))))
 
 (defun defaults/post-init-company ()
   (setq
@@ -133,19 +56,29 @@
     (setq company-flx-limit 100)
     (with-eval-after-load 'company (company-flx-mode t))))
 
-(defun defaults/post-init-yasnippet ()
-  (with-eval-after-load "yasnippet"
-    ;; Now I can complete with TAB while snippet expansion is in progress
-    (setq yas-keymap
-          (let ((map (make-sparse-keymap)))
-            (define-key map (kbd "C-n") 'yas-next-field-or-maybe-expand)
-            (define-key map (kbd "C-p") 'yas-prev-field)
-            (define-key map (kbd "C-g") 'yas-abort-snippet)
-            (define-key map (kbd "C-d") 'yas-skip-and-clear-or-delete-char)
-            map))))
+(defun defaults/post-init-evil ()
+  (spacemacs/set-leader-keys
+    "TAB" 'evil-switch-to-windows-last-buffer)
+  (bind-map-set-keys evil-motion-state-map
+    "C-h" 'evil-window-left
+    "C-j" 'evil-window-down
+    "C-k" 'evil-window-up
+    "C-l" 'evil-window-right)
+  (bind-map-set-keys evil-normal-state-map
+    ":"       'evil-repeat-find-char-reverse
+    "<C-tab>" 'evil-jump-item))
 
-;; disable jshint, jsonlist, and jscs since I prefer eslint checking
+(defun defaults/post-init-evil-args ()
+  (bind-map-set-keys evil-normal-state-map
+    "L" 'evil-forward-arg
+    "H" 'evil-backward-arg)
+  (bind-map-set-keys evil-motion-state-map
+    "L" 'evil-forward-arg
+    "H" 'evil-backward-arg))
+
 (defun defaults/post-init-flycheck ()
+  ;; Disable jshint, jsonlist, and jscs since I prefer eslint checking
+
   ;; Use local eslint executable when it's available in node_modules
   ;; Source: http://emacs.stackexchange.com/questions/21205/flycheck-with-file-relative-eslint-executable
   (defun my/use-eslint-from-node-modules ()
@@ -169,3 +102,71 @@
           flycheck-disabled-checkers
           (append flycheck-disabled-checkers
                   '(javascript-jshint javascript-jscs json-jsonlist)))))
+
+(defun defaults/post-init-gruvbox ()
+  ;; Darker vertical-border for gruvbox
+  (set-face-attribute 'vertical-border nil
+                      :foreground "#1d2021" :background nil))
+
+(defun defaults/post-init-helm ()
+  ;; Enable fuzzy matching for everything
+  (setq helm-completion-in-region-fuzzy-match t
+        helm-mode-fuzzy-match t))
+
+(defun defaults/post-init-helm-dash ()
+  (setq helm-dash-browser-func 'eww))
+
+(defun defaults/post-init-helm-projectile ()
+  (spacemacs/set-leader-keys
+    "/" 'spacemacs/helm-project-do-grep))
+
+(defun defaults/post-init-js2-mode ()
+  ;; Let flycheck handle parse errors
+  (setq js2-mode-show-parse-errors nil
+        js2-mode-show-strict-warnings nil)
+
+  ;; Force js to indent 2 levels
+  (setq js-indent-level tab-width
+        js2-basic-offset tab-width))
+
+(defun defaults/post-init-sh-script ()
+  ;; Use same indentation spaces than 'tab-width
+  (setq sh-indentation tab-width
+        sh-basic-offset tab-width))
+
+(defun defaults/post-init-spaceline ()
+  (setq powerline-default-separator 'utf-8)
+  (custom-set-variables '(powerline-utf-8-separator-left #xe0b0)
+                        '(powerline-utf-8-separator-right #xe0b2))
+  ;; I need to compile spaceline to take the changes
+  (spaceline-compile))
+
+(defun defaults/post-init-subatomic-theme ()
+  (set-face-attribute 'vertical-border nil
+                      :foreground "#232533")
+  (set-face-attribute 'mode-line nil
+                      :box '(:line-width 1 :color "#232533"))
+  (set-face-attribute 'modeline-inactive nil
+                      :box '(:line-width 1 :color "#2a2c3e")))
+
+(defun defaults/post-init-undo-tree ()
+  (setq undo-tree-auto-save-history t
+        undo-tree-history-directory-alist '(("." . "~/.emacs.d/.cache/undo"))))
+
+(defun defaults/post-init-web-mode ()
+  (setq css-indent-offset tab-width
+        web-mode-markup-indent-offset tab-width
+        web-mode-css-indent-offset tab-width
+        web-mode-code-indent-offset tab-width
+        web-mode-attr-indent-offset tab-width))
+
+(defun defaults/post-init-yasnippet ()
+  (with-eval-after-load "yasnippet"
+    ;; Now I can complete with TAB while snippet expansion is in progress
+    (setq yas-keymap
+          (let ((map (make-sparse-keymap)))
+            (define-key map (kbd "C-n") 'yas-next-field-or-maybe-expand)
+            (define-key map (kbd "C-p") 'yas-prev-field)
+            (define-key map (kbd "C-g") 'yas-abort-snippet)
+            (define-key map (kbd "C-d") 'yas-skip-and-clear-or-delete-char)
+            map))))
