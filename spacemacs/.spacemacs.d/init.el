05e310bf0d19edfbf8928667ac17795e9f0ae7d6
eduarbo <eduarbo@gmail.com>
2016-10-25 10:59:31 -0500

    Use normal Hack font

--- spacemacs/.spacemacs.d/init.el
+++ spacemacs/.spacemacs.d/init.el
@@ -191,7 +191,7 @@ values."
    dotspacemacs-colorize-cursor-according-to-state t
    ;; Default font, or prioritized list of fonts. `powerline-scale' allows to
    ;; quickly tweak the mode-line size to make separators look not too crappy.
-   dotspacemacs-default-font '("Knack Nerd Font"
+   dotspacemacs-default-font '("Hack"
                                :size 13
                                :weight normal
                                :width normal
