bcba49b75bceee0f201a51093a733c9fa91a8982
eduarbo <eduarbo@gmail.com>
2016-12-19 11:02:53 -0600

    New kwm and khd config files

--- kwm/.kwm/launch_or_focus.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-# Open a new instance or focus the existing one
-open -a "$1"
-
-# Find oldest process id to focus it with kwm
-win_id=$(/Users/eduarbo/bin/kwmc read windows | grep -i "$1" | head -1 | awk '{print $1;}')
-# remove trailing comma
-win_id=${win_id%?}
-/Users/eduarbo/bin/kwmc window -f id $win_id
