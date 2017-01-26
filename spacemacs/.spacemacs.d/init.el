8b1be6e997f0d90bba40c033f5d3dbbd0a04c5da
eduarbo <eduarbo@gmail.com>
2017-01-25 23:18:19 -0600

    go back to js2-mode

--- spacemacs/.spacemacs.d/init.el
+++ spacemacs/.spacemacs.d/init.el
@@ -389,7 +389,4 @@ you should place your code here."
                          '("xml" "xsd" "sch" "rng" "xslt" "svg" "rss") t)
                         "\\'")
                 'xml-mode))
-
-  ;; better identation for js
-  (push '("\\.js\\'" . react-mode) auto-mode-alist)
   )
