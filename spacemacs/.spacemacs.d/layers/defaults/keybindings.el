b678c80d289fc5f52cef78a2f3199404cd7bf009
eduarbo <eduarbo@gmail.com>
2017-01-26 08:32:27 -0600

    enable notes and crypt private layer and added more bindings

--- spacemacs/.spacemacs.d/layers/defaults/keybindings.el
+++ spacemacs/.spacemacs.d/layers/defaults/keybindings.el
@@ -9,15 +9,19 @@
 ;;; License: GPLv3
 
 (spacemacs/set-leader-keys
+  "TAB"   'evil-jump-item
   "hd,"   'my/describe-foo-at-point
   "TN"    'my/cycle-available-themes
   "n TAB" 'my/narrow-or-widen-dwim
   "cb"    'my/comment-box
   ";"     'eval-expression
+  "."     'spacemacs/alternate-buffer
   "wV"    'split-window-right
   "wv"    'split-window-right-and-focus
   "wS"    'split-window-below
   "ws"    'split-window-below-and-focus)
+
+(global-set-key (kbd "C-,") 'helm-M-x)
 
 (bind-map-set-keys evil-normal-state-map
   "Q" 'fill-paragraph)
