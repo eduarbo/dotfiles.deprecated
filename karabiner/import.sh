faa7cd786e7a8fad2a02b8af2d47b562143adcfa
eduarbo <eduarbo@gmail.com>
2017-03-12 10:50:05 -0600

    update scripts to configure karabiner layouts

---
+++ karabiner/import.sh
@@ -0,0 +1,43 @@
+#!/bin/sh
+
+cli=/usr/local/bin/karabiner
+
+$cli set option.emacsmode_ex_controlW 1
+/bin/echo -n .
+$cli set private.command_l_to_symbols 1
+/bin/echo -n .
+$cli set private.command_r_to_shift_r 1
+/bin/echo -n .
+$cli set private.control_l_to_left_click 1
+/bin/echo -n .
+$cli set private.ctrl_hjkl_to_left_down_up_right 1
+/bin/echo -n .
+$cli set private.f19_to_delete 1
+/bin/echo -n .
+$cli set private.fn_layer 1
+/bin/echo -n .
+$cli set private.left_bracket_to_backquote 1
+/bin/echo -n .
+$cli set private.option_l_to_control_l 1
+/bin/echo -n .
+$cli set private.option_r_to_option_r 1
+/bin/echo -n .
+$cli set private.return_to_parentheses 1
+/bin/echo -n .
+$cli set private.right_bracket_to_backslash 1
+/bin/echo -n .
+$cli set private.shifts_to_parentheses_and_brackets 1
+/bin/echo -n .
+$cli set private.symbols_layer 1
+/bin/echo -n .
+$cli set remap.doublepresscommandQ 1
+/bin/echo -n .
+$cli set remap.launcher_mode_v2 1
+/bin/echo -n .
+$cli set remap.space_to_command_l 1
+/bin/echo -n .
+$cli set repeat.initial_wait 250
+/bin/echo -n .
+$cli set repeat.wait 33
+/bin/echo -n .
+/bin/echo
