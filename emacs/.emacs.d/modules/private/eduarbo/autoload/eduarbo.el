646550d382c86ad2fe926d101e5f9e562a8b1656
eduarbo <eduarbo@gmail.com>
2017-07-05 02:15:46 -0500

    keep all autoloads in one file for now

--- emacs/.emacs.d/modules/private/eduarbo/autoload/eduarbo.el
+++ emacs/.emacs.d/modules/private/eduarbo/autoload/eduarbo.el
@@ -1,6 +1,15 @@
 ;;; private/eduarbo/autoload/eduarbo.el -*- lexical-binding: t; -*-
 
 ;;;###autoload
+(defun +eduarbo/set-workspace--last (name frame)
+  "Save last visited workspace."
+  (let ((old-name (+workspace-current-name)))
+    (when (and (framep frame)
+               (not (string= old-name persp-nil-name))
+               (not (string= old-name +workspace--last)))
+      (setq +workspace--last old-name))))
+
+;;;###autoload
 (defun +eduarbo/install-snippets ()
   "Install my snippets from https://github.com/hlissner/emacs-snippets into
 private/eduarbo/snippets."
