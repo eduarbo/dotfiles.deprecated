f18ae0cc09a08c6ffc40e7afae0b46f4a9e338a2
Eduardo Ruiz <eduarbo@gmail.com>
2017-08-02 17:08:30 -0500

    Press Command+Q twice to Quit Application

--- hammerspoon/.hammerspoon/init.lua
+++ hammerspoon/.hammerspoon/init.lua
@@ -136,3 +136,23 @@ local function moveFocusedWindowToToNextScreen()
   end
 end
 hs.hotkey.bind(MEGA, "tab", moveFocusedWindowToToNextScreen)
+
+-----------------------------------------------
+-- Press Cmd+Q twice to quit
+-----------------------------------------------
+
+local quitModal = hs.hotkey.modal.new('cmd','q')
+
+function quitModal:entered()
+    hs.alert.show("Press Cmd+Q again to quit", 1)
+    hs.timer.doAfter(1, function() quitModal:exit() end)
+end
+
+local function doQuit()
+    local res = hs.application.frontmostApplication():selectMenuItem("^Quit.*$")
+    quitModal:exit()
+end
+
+quitModal:bind('cmd', 'q', doQuit)
+
+quitModal:bind('', 'escape', function() quitModal:exit() end)
