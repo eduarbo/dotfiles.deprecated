4bda87dabf8be21219cc3c27c5383436927f26ad
Eduardo Ruiz <eduarbo@gmail.com>
2017-08-04 03:19:21 -0500

    proud of my push-to-talk widget with feedback at menu bar and hs.alert

--- hammerspoon/.hammerspoon/init.lua
+++ hammerspoon/.hammerspoon/init.lua
@@ -45,5 +45,6 @@ quitModal:bind('', 'escape', function() quitModal:exit() end)
 
 require("hyper")
 require("date-battery")
+require("mic")
 require("netspeed")
 require("window")
---
+++ hammerspoon/.hammerspoon/mic.lua
@@ -0,0 +1,89 @@
+-- When the mic is muted hold f13 key to talk and release to mute again.
+-- You can toggle mute by double tapping the f13 key
+
+local utils = require("utils")
+
+local micKey = 'f13'
+local doubleTap = false
+local pushToTalk = false
+local recentlyTapped = false
+local mic = hs.audiodevice.defaultInputDevice()
+local darkmode_status = hs.osascript.applescript('tell application "System Events"\nreturn dark mode of appearance preferences\nend tell')
+
+local function displayStatus()
+    hs.alert.closeAll()
+    if mic:muted() then
+        hs.alert.show('🎙 Muted', 1)
+    else
+        hs.alert.show('🎙 on Air', 1)
+    end
+end
+
+function updateMicStatus(muted)
+    if muted then
+        micMenubar:setIcon('./icons/mic-off.pdf', true)
+    else
+        micMenubar:setIcon('./icons/mic.pdf', true)
+    end
+end
+
+local function toggleMic()
+    local muted = not mic:muted()
+    mic:setMuted(muted)
+    updateMicStatus(muted)
+end
+
+local doubleTapTimer = hs.timer.delayed.new(0.3, function()
+    recentlyTapped = false
+end)
+
+local function onKeyDown(event)
+    local key = hs.keycodes.map[event:getKeyCode()]
+
+    if key ~= micKey then
+        return false
+    end
+
+    if not pushToTalk then
+        pushToTalk = true
+        toggleMic()
+
+        if recentlyTapped then
+            displayStatus()
+            doubleTap = true
+            recentlyTapped = false
+        else
+            recentlyTapped = true
+            doubleTapTimer:start()
+        end
+    end
+
+    return true
+end
+
+local function onKeyUp(event)
+    local key = hs.keycodes.map[event:getKeyCode()]
+
+    if key ~= micKey then
+        return false
+    end
+
+    pushToTalk = false
+
+    if doubleTap then
+        doubleTap = false
+    else
+        toggleMic()
+    end
+end
+
+displayStatus()
+
+hs.eventtap.new({hs.eventtap.event.types.keyDown}, onKeyDown):start()
+hs.eventtap.new({hs.eventtap.event.types.keyUp}, onKeyUp):start()
+
+if not micMenubar then
+    micMenubar = hs.menubar.new()
+end
+micMenubar:setClickCallback(toggleMic)
+updateMicStatus(mic:muted())
