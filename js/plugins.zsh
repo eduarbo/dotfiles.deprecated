ad9350797f967b0fd93097afaac941c56c891e6d
Eduardo Ruiz <eduarbo@gmail.com>
2017-08-12 03:46:17 -0500

    add completions for npm

--- js/plugins.zsh
+++ js/plugins.zsh
@@ -2,3 +2,7 @@ export NVM_NO_USE=false
 export NVM_LAZY_LOAD=true
 export NVM_AUTO_USE=true
 zplugin light lukechilds/zsh-nvm
+
+if is_interactive; then
+  zplugin light lukechilds/zsh-better-npm-completion
+fi
