8f77568670361ac60f876111a13d63476b17b9ad
Eduardo Ruiz <eduarbo@gmail.com>
2018-03-14 10:02:23 -0600

    postpone loading of nvm plugin by 1 sec after zshrc

--- js/plugins.zsh
+++ js/plugins.zsh
@@ -1,6 +1,7 @@
 export NVM_NO_USE=false
-export NVM_LAZY_LOAD=true
+export NVM_LAZY_LOAD=false
 export NVM_AUTO_USE=true
+zplugin ice lucid wait'1'
 zplugin light lukechilds/zsh-nvm
 
 if is_interactive; then
