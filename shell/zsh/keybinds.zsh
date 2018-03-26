51905e1d629f651308cd72af9522665e09ebc032
Eduardo Ruiz Macias <eduarbo@gmail.com>
2018-03-26 12:43:43 -0600

    remove jk binding

--- shell/zsh/keybinds.zsh
+++ shell/zsh/keybinds.zsh
@@ -8,7 +8,6 @@ autoload -U is-at-least
 
 ## vi-mode ###############
 bindkey -v
-bindkey -M viins 'jk' vi-cmd-mode
 bindkey -M viins ' ' magic-space
 # bindkey -M viins '^I' expand-or-complete-prefix
 
