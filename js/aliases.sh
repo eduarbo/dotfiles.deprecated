727c3846e771496fbff7b7cbee42b757fe83c1d4
Eduardo Ruiz <eduarbo@gmail.com>
2018-03-26 01:17:49 -0600

    refactor js module and renamed to node

--- js/aliases.sh
+++
@@ -1,1 +0,0 @@
-alias nvmupgrade='cd "$NVM_DIR" && git fetch origin && git checkout `git describe --abbrev=0 --tags` && cd -'
--- js/env.sh
+++
@@ -1,4 +0,0 @@
-export NVM_DIR="$HOME/.nvm"
-export NVM_SYMLINK_CURRENT=true
-
-addpath "$NVM_DIR"/current/bin
