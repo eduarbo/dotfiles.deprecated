a69ec92abd94793be056df8fd09ef9029f9bb840
Eduardo Ruiz Macias <eduarbo@gmail.com>
2018-03-29 01:44:57 -0600

    do not run compdef

--- shell/zsh/aliases.zsh
+++ shell/zsh/aliases.zsh
@@ -5,7 +5,7 @@ zman() { PAGER="less -g -s '+/^       "$1"'" man zshall; }
 r() {
   local time=$1; shift
   sched "$time" "notify-send --urgency=critical 'Reminder' '$@'; ding";
-}; compdef r=sched
+}
 
 alias q="exit"
 alias clr="clear"
