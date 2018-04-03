798389fde5fdbde1c2059896d1107bfb62166d9f
Eduardo Ruiz Macias <eduarbo@gmail.com>
2018-04-02 22:08:28 -0500

    aliases to search with ag and rg

--- shell/zsh/aliases.zsh
+++ shell/zsh/aliases.zsh
@@ -25,7 +25,9 @@ alias fgrep="fgrep --color=auto"
 alias egrep="egrep --color=auto"
 
 alias ag="noglob ag -p $XDG_CONFIG_HOME/ag/agignore"
+alias agg='ag -S --hidden --line-number'
 alias rg='noglob rg'
+alias rgg='rg -S --hidden --line-number'
 
 alias mk="make"
 
