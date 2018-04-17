25ff3e1f1a185952a34656022e03d1aaf05eb77e
Eduardo Ruiz Macias <eduarbo@gmail.com>
2018-04-17 13:31:33 -0500

    manually add nodenv shims to PATH so emacs is aware of that

--- dev/node/env.zsh
+++ dev/node/env.zsh
@@ -2,5 +2,5 @@ export NPM_CONFIG_USERCONFIG=$XDG_CONFIG_HOME/npm/config
 export NODE_REPL_HISTORY=$XDG_CACHE_HOME/node/repl_history
 
 export NODENV_ROOT=$XDG_DATA_HOME/nodenv
-path=( $NODENV_ROOT/bin $path )
+path=( $NODENV_ROOT/bin $NODENV_ROOT/shims $path )
 _cache nodenv init - --no-rehash
