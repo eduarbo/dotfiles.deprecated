99750915a646f0a09c52ae7b84880ad0fd7749a9
Eduardo Ruiz Macias <eduarbo@gmail.com>
2018-08-09 17:38:31 -0500

    match init.el with latest doom version

--- editor/emacs/doom/init.el
+++ editor/emacs/doom/init.el
@@ -1,11 +1,6 @@
 ;;; init.el -*- lexical-binding: t; -*-
 
-(package! emacs-snippets :disable t)
-
 (doom! :feature
-       (popup            ; tame sudden yet inevitable temporary windows
-        +all             ; catch all popups that start with an asterix
-        +defaults)       ; default popup rules
       ;debugger          ; FIXME stepping through code, to help you add bugs
        eval              ; run code, run (also, repls)
        (evil +everywhere); come to the dark side, we have cookies
@@ -13,22 +8,20 @@
        (lookup           ; helps you navigate your code and documentation
         +devdocs         ; ...on devdocs.io online
         +docsets)        ; ...or in Dash docsets locally
-      ;services          ; TODO managing external services & code builders
        snippets          ; my elves. They type so I don't have to
        spellcheck        ; tasing you for misspelling mispelling
        (syntax-checker   ; tasing you for every semicolon you forget
         +childframe)     ; use childframes for error popups (Emacs 26+ only)
-       version-control   ; remember, remember that commit in November
        workspaces        ; tab emulation, persistence & separate workspaces
 
        :completion
        (company          ; the ultimate code completion backend
-      ; +auto            ; as-you-type code completion
-        +childframe)     ; a nicer company UI (Emacs 26+ only)
-      ;helm              ; the *other* search engine for love and life
+        +auto)           ; as-you-type code completion
+       (helm             ; the *other* search engine for love and life
+        +fuzzy)          ; enable fuzzy search backend for helm
       ;ido               ; the other *other* search engine...
-       (ivy              ; a search engine for love and life
-        +childframe)     ; uses childframes for popups (Emacs 26+ only)
+      ;(ivy              ; a search engine for love and life
+      ; +fuzzy)          ; enable fuzzy search backend for ivy
 
        :ui
        doom              ; what makes DOOM look the way it does
@@ -36,39 +29,58 @@
        doom-modeline     ; a snazzy Atom-inspired mode-line
        doom-quit         ; DOOM quit-message prompts when you quit Emacs
        evil-goggles      ; display visual hints when editing in evil
+      ;fci               ; a `fill-column' indicator
        hl-todo           ; highlight TODO/FIXME/NOTE tags
+      ;modeline          ; snazzy, Atom-inspired modeline, plus API
        nav-flash         ; blink the current line after jumping
+      ;neotree           ; a project drawer, like NERDTree for vim
+       treemacs          ; a project drawer, like neotree but cooler
+       (popup            ; tame sudden yet inevitable temporary windows
+        +all             ; catch all popups that start with an asterix
+        +defaults)       ; default popup rules
+      ;pretty-code       ; replace bits of code with pretty symbols
       ;tabbar            ; FIXME an (incomplete) tab bar for Emacs
       ;unicode           ; extended unicode support for various languages
+       vc-gutter         ; vcs diff in the fringe
        vi-tilde-fringe   ; fringe tildes to mark beyond EOB
-      ;window-select     ; visually switch windows
+       window-select     ; visually switch windows
+
+       :editor
+       multiple-cursors  ; editing in many places at once
+      ;parinfer          ; turn lisp into python, sort of
+       rotate-text       ; cycle region at point between text candidates
+
+       :emacs
+       dired             ; making dired pretty [functional]
+       ediff             ; comparing files in Emacs
+       electric          ; smarter, keyword-based electric-indent
+      ;eshell            ; a consistent, cross-platform shell (WIP)
+       hideshow          ; basic code-folding support
+       imenu             ; an imenu sidebar and searchable code index
+      ;term              ; terminals in Emacs
+       vc                ; version-control and Emacs, sitting in a tree
 
        :tools
-       dired             ; making dired pretty [functional]
        editorconfig      ; let someone else argue about tabs vs spaces
-       ein               ; tame Jupyter notebooks with emacs
-       electric-indent   ; smarter, keyword-based electric-indent
-      ;eshell            ; a consistent, cross-platform shell (WIP)
+      ;ein               ; tame Jupyter notebooks with emacs
       ;gist              ; interacting with github gists
-       imenu             ; an imenu sidebar and searchable code index
-      ;impatient-mode    ; show off code over HTTP
        macos             ; MacOS-specific commands
       ;make              ; run make tasks from Emacs
        magit             ;
-       neotree           ; a project drawer, like NERDTree for vim
-       password-store    ; password manager for nerds
+      ;password-store    ; password manager for nerds
       ;pdf               ; pdf enhancements
+      ;prodigy           ; FIXME managing external services & code builders
       ;rgb               ; creating color strings
-       rotate-text       ; cycle region at point between text candidates
-       term              ; terminals in Emacs
-       tmux              ; an API for interacting with tmux
+      ;tmux              ; an API for interacting with tmux
       ;upload            ; map local to remote projects via ssh/ftp
+      ;wakatime
 
        :lang
       ;assembly          ; assembly for fun or debugging
-      ;cc                ; C/C++/Obj-C madness
-      ;crystal           ; ruby at the speed of c
+      ;(cc +irony +rtags); C/C++/Obj-C madness
       ;clojure           ; java with a lisp
+      ;common-lisp       ; if you've seen one lisp, you've seen them all
+      ;crystal           ; ruby at the speed of c
       ;csharp            ; unity, .NET, and mono shenanigans
        data              ; config/data formats
       ;erlang            ; an elegant language for a more civilized age
@@ -94,18 +106,20 @@
         +babel           ; running code in org
         +capture         ; org-capture in and outside of Emacs
         +export          ; Exporting org to whatever you want
-        +present         ; Emacs for presentations
-        +publish)        ; Emacs+Org as a static site generator
+        +present)        ; Emacs for presentations
       ;perl              ; write code no one else can comprehend
       ;php               ; perl's insecure younger brother
       ;plantuml          ; diagrams for confusing people more
-       purescript        ; javascript, but functional
+      ;purescript        ; javascript, but functional
        python            ; beautiful is better than ugly
+      ;qt                ; the 'cutest' gui framework ever
+      ;racket            ; a DSL for DSLs
        rest              ; Emacs as a REST client
-      ;ruby              ; 1.step do {|i| p "Ruby is #{i.even? ? 'love' : 'life'}"}
+       ruby              ; 1.step do {|i| p "Ruby is #{i.even? ? 'love' : 'life'}"}
       ;rust              ; Fe2O3.unwrap().unwrap().unwrap().unwrap()
       ;scala             ; java, but good
-       sh                ; she sells (ba|z)sh shells on the C xor
+       (sh +fish)        ; she sells (ba|z)sh shells on the C xor
+      ;solidity          ; do you need a blockchain? No.
       ;swift             ; who asked for emoji variables?
        web               ; the tubes
 
@@ -114,14 +128,22 @@
        ;; should be loaded late.
        :app
       ;(email +gmail)    ; emacs as an email client
-      ;irc               ; how neckbeards socialize
+       irc               ; how neckbeards socialize
       ;(rss +org)        ; emacs as an RSS reader
       ;twitter           ; twitter client https://twitter.com/vnought
       ;(write            ; emacs as a word processor (latex + org + markdown)
       ; +wordnut         ; wordnet (wn) search
       ; +langtool)       ; a proofreader (grammar/style check) for Emacs
 
+       :collab
+      ;floobits          ; peer programming for a price
+      ;impatient-mode    ; show off code over HTTP
+
        :config
+       ;; For literate config users. This will tangle+compile a config.org
+       ;; literate config in your `doom-private-dir' whenever it changes.
+      ;literate
+
        ;; The default module set reasonable defaults for Emacs. It also provides
        ;; a Spacemacs-inspired keybinding scheme, a custom yasnippet library,
        ;; and additional ex commands for evil-mode. Use it as a reference for
