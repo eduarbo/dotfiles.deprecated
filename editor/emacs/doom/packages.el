c49057a1e214e86c55f82c5eaa91b225a5a0cf76
Eduardo Ruiz Macias <eduarbo@gmail.com>
2019-01-17 13:20:17 -0600

    Disable evil-escape package

---
+++ editor/emacs/doom/packages.el
@@ -0,0 +1,5 @@
+;; -*- no-byte-compile: t; -*-
+;;; ~/.dotfiles/editor/emacs/doom/packages.el
+
+;; My ESC key is very reachable so there is no need for a key sequence to escape
+(package! evil-escape :disable t)
