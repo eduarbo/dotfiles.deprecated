268307af106530e6eba2586890c332e2049605c8
Eduardo Ruiz Macias <eduarbo@gmail.com>
2019-01-22 04:07:05 -0600

    Add DOOM banner
    and refactor +mi-dashboard-widget-banner

--- editor/emacs/doom/+dashboard.el
+++ editor/emacs/doom/+dashboard.el
@@ -1,26 +1,37 @@
 ;;; ~/.dotfiles/editor/emacs/doom/banners.el -*- lexical-binding: t; -*-
+(defvar +mi-dashboard-banner "momacs")
 
 (setq +doom-dashboard-functions
-      '(eduarbo-dashboard-widget-banner
+      '(+mi-dashboard-widget-banner
         doom-dashboard-widget-shortmenu
         doom-dashboard-widget-loaded
         doom-dashboard-widget-footer))
 
-(defun mi-dashboard-widget-banner ()
-  "Insert the ascii banner contain in file and center it in the window.
-FILE: the path to the file containing the banner."
-  (let* ((banner-width 0)
-         (banner (with-temp-buffer
-                   (insert-file-contents (eduarbo--get-banner-path "momacs"))
-                   (while (not (eobp))
-                     ;; Get max line lenght
-                     (let ((line-length (- (line-end-position) (line-beginning-position))))
-                       (if (< banner-width line-length)
-                           (setq banner-width line-length)))
-                     (forward-line 1))
-                   (buffer-string))))
-    (let ((margin (max 0 (floor (/ (- +doom-dashboard--width banner-width) 2)))))
-      (mapc (lambda (line)
-              (insert (propertize (concat (make-string margin ?\s) line) 'face 'font-lock-comment-face) " ")
-              (insert "\n"))
-            (split-string banner "\n" t)))))
+(defun +mi-dashboard-widget-banner ()
+  "Insert ASCII banner contained in file and center it."
+  (let* ((banner-lines (split-string (+mi--dashboard-get-banner) "\n"))
+         (banner-width (seq-reduce #'+mi--dashboard-get-banner-width banner-lines 0))
+         (margin (max 0 (floor (/ (- +doom-dashboard--width banner-width) 2)))))
+    (mapc (lambda (line)
+            (insert (propertize (concat (make-string margin ?\s) line)
+                                'face 'font-lock-comment-face) "\n"))
+          banner-lines)))
+
+
+;;
+;; Helpers
+
+(defun +mi--dashboard-get-banner ()
+  "Load banner from file and return as a string."
+  (condition-case _
+      (with-temp-buffer
+        (insert-file-contents
+         (concat doom-private-dir "banners/" (format "%s.txt" +mi-dashboard-banner)))
+        (buffer-string))
+    (file-error "")))
+
+(defun +mi--dashboard-get-banner-width (banner-width line)
+  "Get length of longest line"
+  (if (< banner-width (length line))
+      (length line)
+    banner-width))
